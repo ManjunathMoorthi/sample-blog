db0d8d1baca311331be01b591269d6aab9426f6d
Manjunath Moorthi <manjunathmoorthi@gmail.com>
2017-06-23 18:54:49 -0500

    Update main.ts

--- app/main.ts
+++ app/main.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { RouterModule} from '@angular/router';
+import { SignInComponent } from './SignIn/signin.component';
+import { SignUpComponent } from './SignUp/signup.component';
+import { routes } from './app.routes';
+
+
 
 @NgModule({
-  imports: [ BrowserModule, FormsModule],
-  declarations: [ AppComponent],
+  imports: [ BrowserModule, FormsModule, RouterModule.forRoot(routes)],
+  declarations: [ AppComponent, SignInComponent, SignUpComponent],
   bootstrap: [ AppComponent ]
 })
 class AppModule {}
